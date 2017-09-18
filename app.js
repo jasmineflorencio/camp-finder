d2134421a2d1cfd92567ab860a00253c17c9b689
Jasmine Florencio <jasmineflorencio@gmail.com>
2017-09-18 13:21:05 -0700

    add user authentication

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-var express     = require("express"),
-    app         = express(),
-    bodyParser  = require("body-parser"),
-    mongoose    = require("mongoose"),
-    Campground  = require("./models/campground"),
-    Comment     = require("./models/comment"),
-    seedDB      = require("./seeds");
+var express       = require("express"),
+    app           = express(),
+    bodyParser    = require("body-parser"),
+    mongoose      = require("mongoose"),
+    passport      = require("passport"),
+    localStrategy = require("passport-local"),
+    Campground    = require("./models/campground"),
+    Comment       = require("./models/comment"),
+    User          = require("./models/user"),
+    seedDB        = require("./seeds");
 
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost/yelp_camp", {useMongoClient: true});
@@ -13,24 +16,37 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 seedDB();
 
-// var campgrounds = [
-//   {name: "Salmon Creek", image: "https://source.unsplash.com/5Rhl-kSRydQ"},
-//   {name: "Granite Hill", image: "https://source.unsplash.com/nYiCGILaoVI"},
-//   {name: "Eagle's Perch", image: "https://source.unsplash.com/K9olx8OF36A"},
-//   {name: "Windy River", image: "https://source.unsplash.com/qelGaL2OLyE"},
-//   {name: "Watery Heights", image: "https://source.unsplash.com/FHjmmLeEipo"},
-//   {name: "Soaring Cliff", image: "https://source.unsplash.com/sK1hW5knKkw"}
-// ]
+app.use(require("express-session")({
+  secret: "learning more about setting up passport!",
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+  res.locals.currentUser = req.user;
+  next();
+});
+
 app.get("/", function(req, res){
   res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res){
+  req.user
   Campground.find({}, function(err, camps){
     if (err) {
       console.log(err);
     } else {
-      res.render("campgrounds/index", {campgrounds: camps});
+      res.render("campgrounds/index",
+      {
+        campgrounds: camps,
+        // currentUser: req.user
+      });
     }
   });
 });
@@ -63,7 +79,7 @@ app.get("/campgrounds/:id", function(req, res){
   });
 });
 
-app.get("/campgrounds/:id/comments/new", function(req, res){
+app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res){
   Campground.findById(req.params.id, function(err, campground){
     if (err) {
       console.log(err);
@@ -73,7 +89,7 @@ app.get("/campgrounds/:id/comments/new", function(req, res){
   });
 });
 
-app.post("/campgrounds/:id/comments", function(req, res){
+app.post("/campgrounds/:id/comments", isLoggedIn, function(req, res){
   Campground.findById(req.params.id, function(err, campground){
     if (err) {
       console.log(err);
@@ -90,6 +106,48 @@ app.post("/campgrounds/:id/comments", function(req, res){
     }
   });
 });
+
+// Auth Routes
+app.get("/register", function(req, res){
+  res.render("register");
+});
+
+app.post("/register", function(req, res){
+  var newUser = new User({username: req.body.username});
+  User.register(newUser, req.body.password, function(err, user){
+    if (err) {
+      console.log(err);
+      return res.render("register");
+    }
+
+    passport.authenticate("local")(req, res, function(){
+      res.redirect("/campgrounds");
+    });
+  });
+});
+
+app.get("/login", function(req, res){
+  res.render("login");
+});
+
+app.post("/login", passport.authenticate("local",
+  {
+    successRedirect: "/campgrounds",
+    failureRedirect: "/login"
+  }), function(req, res){
+});
+
+app.get("/logout", function(req, res){
+  req.logout();
+  res.redirect("/campgrounds");
+});
+
+function isLoggedIn(req, res, next){
+  if(req.isAuthenticated()){
+    return next();
+  }
+  res.redirect("/login");
+};
 
 app.listen(3000, function(){
   console.log("Here we go!")
